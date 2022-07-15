f376c9f99b522b781efb8df882be3cad12b47272
Ludwigvsch <lvschoenfeldt@gmx.de>
2022-07-15 14:33:05 -0700

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.*;
+
+public class Main{
+    //print hello world
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        //ask for name
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        System.out.println("Hello " + name);
     }
 }
